00b0d45c01740dbfec69244008dccf50be8cfdea
Adam Halley <adahal01@student.wintec.ac.nz>
2018-07-19 00:57:31 +0000

    First Commit

---
+++ hello-world.php
@@ -0,0 +1,115 @@
+<html><body>
+
+<?php
+// A simple web site in Cloud9 that runs through Apache
+// Press the 'Run' button on the top to start the web server,
+// then click the URL that is emitted to the Output tab of the console
+
+echo '<h1>Hello world from Adam!</h1>';
+
+$adam = 'dumb';
+echo $adam;
+
+$number1 = 5;
+$number2 = 10;
+
+echo '<br>';
+$number3 = $number1 * $number2;
+echo $number3;
+
+echo '<h2>suhhh dud</h2>';
+
+?>
+
+<div class="box">
+    <div class="nav">
+        <h1 class="title">YOYOYOYOYOYO SUP BRO DIS IS MY WEBSITE</h1>
+    </div>
+    <div class="lyrics">
+        <p>
+            We're no strangers to love
+You know the rules and so do I
+A full commitment's what I'm thinking of
+You wouldn't get this from any other guy
+I just wanna tell you how I'm feeling
+Gotta make you understand
+Never gonna give you up
+Never gonna let you down
+Never gonna run around and desert you
+Never gonna make you cry
+Never gonna say goodbye
+Never gonna tell a lie and hurt you
+We've known each other for so long
+Your heart's been aching but you're too shy to say it
+Inside we both know what's been going on
+We know the game and we're gonna play it
+And if you ask me how I'm feeling
+Don't tell me you're too blind to see
+Never gonna give you up
+Never gonna let you down
+Never gonna run around and desert you
+Never gonna make you cry
+Never gonna say goodbye
+Never gonna tell a lie and hurt you
+Never gonna give you up
+Never gonna let you down
+Never gonna run around and desert you
+Never gonna make you cry
+Never gonna say goodbye
+Never gonna tell a lie and hurt you
+Never gonna give, never gonna give
+(Give you up)
+(Ooh) Never gonna give, never gonna give
+(Give you up)
+We've known each other for so long
+Your heart's been aching but you're too shy to say it
+Inside we both know what's been going on
+We know the game and we're gonna play it
+I just wanna tell you how I'm feeling
+Gotta make you understand
+Never gonna give you up
+Never gonna let you down
+Never gonna run around and desert you
+Never gonna make you cry
+Never gonna say goodbye
+Never gonna tell a lie and hurt you
+Never gonna give you up
+Never gonna let you down
+Never gonna run around and desert you
+Never gonna make you cry
+Never gonna say goodbye
+Never gonna tell a lie and hurt you
+Never gonna give you up
+Never gonna let you down
+Never gonna run around and desert you
+Never gonna make you cry
+        </p>
+    </div>
+    
+</div>
+
+<style>
+    .box {
+        background-color: red;
+        width: 100%;
+        height: 100%;
+    }
+    .nav {
+        background-color: #42f468;
+        width: 100%;
+    }
+    .title {
+        text-align: center;
+        padding-top: 40px;
+        padding-bottom: 40px;
+        color: pink;
+        font-family: Comic Sans MS;
+        font-size: 50px;
+    }
+    .lyrics {
+        width: 300px;
+    }
+</style>
+
+</body>
+</html>
